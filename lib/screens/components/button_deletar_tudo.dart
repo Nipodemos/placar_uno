fd2625726446bc79eb34156639c5307ebef69518
nipodemos13@gmail.com <Nipodemos>
2020-05-25 00:16:57 -0300

    removido o botão DELETAR TUDO da tela antiga
    esse arquivo está com erros, vou corrigir depois

---
+++ lib/screens/components/button_deletar_tudo.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RaisedButtonDeletarTudo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text('Deletar TUDO'),
+      onPressed: () {
+        if (placarUnoBox.get('alan') != null) {
+          placarUnoBox.put('alan', {
+            'placarTotal': 0,
+            'primeiroLugar': 0,
+            'segundoLugar': 0,
+            'terceiroLugar': 0,
+            'quartoLugar': 0
+          });
+        }
+        if (placarUnoBox.get('guilherme') != null) {
+          placarUnoBox.put('guilherme', {
+            'placarTotal': 0,
+            'primeiroLugar': 0,
+            'segundoLugar': 0,
+            'terceiroLugar': 0,
+            'quartoLugar': 0
+          });
+        }
+        if (placarUnoBox.get('joao') != null) {
+          placarUnoBox.put('joao', {
+            'placarTotal': 0,
+            'primeiroLugar': 0,
+            'segundoLugar': 0,
+            'terceiroLugar': 0,
+            'quartoLugar': 0
+          });
+        }
+        if (placarUnoBox.get('tomas') != null) {
+          placarUnoBox.put('tomas', {
+            'placarTotal': 0,
+            'primeiroLugar': 0,
+            'segundoLugar': 0,
+            'terceiroLugar': 0,
+            'quartoLugar': 0
+          });
+        }
+      },
+    );
+  }
+}

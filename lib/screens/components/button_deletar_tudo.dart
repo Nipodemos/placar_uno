3bb1c51bbc24e3a93801f0529f41f7776a0054ca
nipodemos13@gmail.com <Nipodemos>
2020-05-25 00:20:13 -0300

    consertado botaõ deletar tudo para sumir os erros

--- lib/screens/components/button_deletar_tudo.dart
+++ lib/screens/components/button_deletar_tudo.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class RaisedButtonDeletarTudo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Box placarUnoBox = Hive.box('placarUno');
     return RaisedButton(
       child: Text('Deletar TUDO'),
       onPressed: () {
